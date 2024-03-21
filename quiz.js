5f89d9b23b1ce7b7dc139aa63b96463c9d98c7c7
TatendaLatmore <159183456+TatendaLatmore@users.noreply.github.com>
2024-03-21 13:34:37 -0400

    hd

--- quiz.js
+++ quiz.js
@@ -2,8 +2,39 @@ const prompt = require('prompt-sync')();
 
 //Program 1: Temperature Converter
 
+console.log("Welcome to my tempurature converter app");
+
+let fahrenheit = prompt("Input the fahrenheit tempurrature");
+let celsius =(fahrenheit - 32) * 5/9;
+
+console.log("Your tempurature in celsius " + celsius.toFixed(2));
+
 //Program 2: Grade Calculator
+console.log("Hi, i'm here to calculate your grade");
+
+let percentage = prompt("Enter your grade here");
+
+if (percentage>= 90);
+grade = A;
+
+if (percentage> 80);
+grade = B;
+
+if (percentage>= 70);
+grade = C;
+
+if (percentage>= 60);
+grade = D;
+
+if (percentage<60);
+grade = F;
+
+
+
 
 //Program 3: Leap Year Checker
+
+console.log("welcome to your Leap Year Checker");
+let year = prompt("Enter the year");
 
 //Program 4: Largest Number Finder
